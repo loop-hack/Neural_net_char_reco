3ecff41110e361a1c8ce1922e852c2077a346869
loop-hack <timetraveller1732@gmail.com>
2025-07-23 19:43:01 +0530

    created load fn

--- network2.py
+++ network2.py
@@ -182,3 +182,17 @@ def save(self, filename):
         f = open(filename, "w")
         json.dump(data, f)
         f.close()
+
+def load(filename):
+    """Load a neural network from the file ``filename``.  Returns an
+    instance of Network.
+
+    """
+    f = open(filename, "r")
+    data = json.load(f)
+    f.close()
+    cost = getattr(sys.modules[__name__], data["cost"])
+    net = Network(data["sizes"], cost=cost)
+    net.weights = [np.array(w) for w in data["weights"]]
+    net.biases = [np.array(b) for b in data["biases"]]
+    return net
