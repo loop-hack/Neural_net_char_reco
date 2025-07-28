a63c472ca8bbda206de49d44de11ceba7be90038
loop-hack <timetraveller1732@gmail.com>
2025-07-28 17:33:37 +0530

    created c file just for testing

--- ankit.c
+++ ankit.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+
+int facto(int x){
+    int i ;
+    int fact_a = 1;
+    for(i = 1; i<x; i++){
+        fact_a *= i;
+    }
+    return fact_a;
+}
 int main(){
-    printf("Hey");
+
+    int a = 10;
+    int y = facto(a);
+    printf("%d",y);
+    return 0;
 }
